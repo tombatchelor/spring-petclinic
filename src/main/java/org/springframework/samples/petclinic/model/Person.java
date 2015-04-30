8d8413bb814df7dc2a64e4158e5f0317a12a45ca
Tom Batchelor <tbatchelor@xebialabs.com>
2015-04-30 11:17:16 -0400

    add issue

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -28,18 +28,13 @@ import org.hibernate.validator.constraints.NotEmpty;
 @MappedSuperclass
 public class Person extends BaseEntity {
 
-    @Column(name = "first_name")
+    @Column(name = "first_name2")
     @NotEmpty
     protected String firstName;
 
     @Column(name = "last_name")
     @NotEmpty
     protected String lastName;
-
-    public Person () {
-        super();
-        firstName="Jeff";
-    }
 
     public String getFirstName() {
         return this.firstName;
